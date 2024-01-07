aeaebaf8adcb488a0b468602d378a0330ae00081
Chris Welch <chris.welch@currenthealth.com>
2024-01-07 01:23:40 +0000

    updating .gitignore

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welchie Learn React
         </a>
       </header>
     </div>
