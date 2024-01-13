cb9eec9feca14d2ba9d9a1a4e191118a4028a7f4
Chris Welch <chris.welch@currenthealth.com>
2024-01-13 13:41:08 +0000

    Updating to have check boxes for Events delete

--- src/pages/EventsResultsPage.js
+++ src/pages/EventsResultsPage.js
@@ -11,26 +11,61 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
 
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 const EventsResultsPage = ({rows}) => {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [state, setState] = React.useState({
+      row1: true,
+      row2: false,
+      row3: false,
+    });
 
+   const { row1, row2, row3 } = state;
+
+  var checkedRows = [];
+  checkedRows[0] = "Test";
+  var checkedRowsCount = 0;
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
   const deleteRows = (rows) => {
-      console.log("Deleting checked rows");
-    }
+    //alert(checkedRows);
+  };
+
+//    for (let i = 0; i < rows.length; i++) {
+//        if ( rows[i].)
+//    text += cars[i] + "<br>";
+//  }
+//      for(rows)
+//      console.log("Deleting checked rows");
+//    }
+
+  const checkRow = (event) => {
+    checkedRowsCount++;
+    checkedRows[checkedRowsCount] = "clicked row " + event.target;
+
+  };
+
+  const handleCheckedChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setState({
+        ...state,
+        [event.target.name]: event.target.checked,
+      });
+    };
 
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(+event.target.value);
     setPage(0);
   };
 
-
-    return (  
+return (
       
 
 <Paper sx={{ width: '100%' }} style={{width:1000}}>
@@ -61,7 +96,15 @@ const EventsResultsPage = ({rows}) => {
 
           >
 
-            <TableCell align="left" ><Checkbox /> </TableCell>
+            <TableCell align="left" >
+            <FormControlLabel
+                        control={
+             <Checkbox  checked={row2} onChange={handleCheckedChange} name="{row.uid}" />
+                        }
+                        label={row.uid}
+                      />
+
+            </TableCell>
             <TableCell align="left">{row.name}</TableCell>
             <TableCell align="left">{row.venue}</TableCell>
             <TableCell align="left">{row.startDate}</TableCell>
@@ -77,6 +120,8 @@ const EventsResultsPage = ({rows}) => {
     </Table>
     <Button variant="contained" onClick={deleteRows({rows})}
     class="input-button" style={{width: 100, height: 50}}>Delete</Button>
+
+    <TableCell >Rows {checkedRows}</TableCell>
   </TableContainer>
    <TablePagination
    rowsPerPageOptions={[10, 25, 100]}
