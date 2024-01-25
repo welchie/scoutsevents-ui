6ada1be11f036e85f1a60e993dff901ec2593cb4
Chris Welch <chris.welch@currenthealth.com>
2024-01-25 01:00:11 +0000

    upadted to include check-in/out

--- src/pages/PersonPage.js
+++ src/pages/PersonPage.js
@@ -16,6 +16,8 @@ import { useState } from "react";
 import useUser from '../hooks/useUser';
 import LoginPage from "./LoginPage";
 import Button from '@mui/material/Button';
+import DoneIcon from '@mui/icons-material/Done';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 const PersonPage = () =>
@@ -28,15 +30,19 @@ const PersonPage = () =>
 
 
  const [person, setPerson] = useState(null);
+ const [eventAttendee, setEventAttendee] = useState(null);
+
  var [errorMessage, setErrorMessage] = useState('');
 
- const { REACT_APP_API_BASE_URL, REACT_APP_API_HEADERS, REACT_APP_API_BASE_LOCAL_URL, NODE_ENV } = process.env;
+ const { REACT_APP_API_BASE_URL, REACT_APP_API_HEADERS, REACT_APP_API_BASE_LOCAL_URL, NODE_ENV, REACT_APP_MODE } = process.env;
 
  const API_URL =
      NODE_ENV === 'production' ? process.env.REACT_APP_API_BASE_URL :process.env.REACT_APP_API_BASE_LOCAL_URL ;
 
-   const API_HEADERS =
-       NODE_ENV === 'production' ? process.env.REACT_APP_API_HEADERS: window.API_URL ;
+const API_HEADERS =
+   NODE_ENV === 'production' ? process.env.REACT_APP_API_HEADERS: window.API_URL ;
+
+const DEV_MODE =  process.env.REACT_APP_MODE;
 
  const getPersonDetails = async() => {
         try{
@@ -53,12 +59,66 @@ const PersonPage = () =>
         }
     };
 
-   getPersonDetails();
+     const getEventAttendeeDetails = async() => {
+            try{
 
-   const checkin = () =>
+                if (eventAttendee === null)
+                {
+                    var url = API_URL + "/eventattendee/findbyperson?personuid=" + uid ;
+                    const response = await axios.get(url,{REACT_APP_API_HEADERS});
+                    setEventAttendee(response.data.EventAttendee[0]);
+                    setErrorMessage('');
+                 }
+            }
+            catch (e) {
+                setErrorMessage(e.message);
+            }
+        };
+
+   getPersonDetails();
+   getEventAttendeeDetails();
+
+   const checkin = async() =>
    {
-       alert("Checking in");
+       //alert("Checking in");
+
+        try{
+
+           if (eventAttendee != null)
+           {
+               var checkIn = "true";
+               var url = API_URL + "/eventattendee/checkin?eventUID=" + eventAttendee.eventUid + "&personUID=" + person.uid + "&checkIn=" + checkIn ;
+               //alert(url);
+               const response = await axios.get(url,{REACT_APP_API_HEADERS});
+               setEventAttendee(response.data.EventAttendee);
+               setErrorMessage('');
+            }
+       }
+       catch (e) {
+           setErrorMessage(e.message);
+       }
    }
+
+    const checkout = async() =>
+      {
+            //alert("Checking out");
+
+                  try{
+
+                     if (eventAttendee != null)
+                     {
+                         var checkIn = "false";
+                         var url = API_URL + "/eventattendee/checkin?eventUID=" + eventAttendee.eventUid + "&personUID=" + person.uid + "&checkIn=" + checkIn ;
+                         //alert(url);
+                         const response = await axios.get(url,{REACT_APP_API_HEADERS});
+                         setEventAttendee(response.data.EventAttendee);
+                         setErrorMessage('');
+                      }
+                 }
+                 catch (e) {
+                     setErrorMessage(e.message);
+                 }
+      }
 
  return (
           <>
@@ -86,10 +146,34 @@ const PersonPage = () =>
           <br/>Relationship to {person.firstName}: {person.emergencyRelationship}
 
           <br/>
+          <br/><b>Checked In To Camp </b>
 
+          {eventAttendee.checkedIn === 'false' ? <b style={{color:"red"}}> <CloseIcon color="red" fontSize="large"/></b> :
+          <><b style={{color:"green"}}> <DoneIcon color="green" fontSize="large"/></b></> }
+
+          <br/>
+             {DEV_MODE === 'dev' ?
+             <>
+             <br/>Person UID: {person.uid}
+             <br/>Event UID: {eventAttendee.eventUid}
+             <br/>
+             </>
+             :
+             null}
+
+            {eventAttendee.checkedIn === "false" ?
+            <>
+            <br/>
             <Button variant="contained" onClick={checkin}
-                class="input-button" style={{width: 100, height: 50}}>Check In</Button>
-
+                class="input-button" style={{width: 125, height: 50}}>Check In</Button>
+            </>
+            :
+            <>
+            <br/>
+            <Button variant="contained" onClick={checkout}
+                              class="input-button" style={{width: 125, height: 50}}>Check Out</Button>
+            </>
+            }
 
           </>
           )
