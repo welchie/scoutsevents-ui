8843125696f55302aee2e884fe9323b1225200c9
Chris Welch <welchie99@gmail.com>
2024-07-20 18:29:21 +0100

    Updating docker build file

--- docker-build.sh
+++ docker-build.sh
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
-docker image build -t welchie99/scoutsevents-ui:0.0.18-SNAPSHOT-PROD .
-=======
-docker image build -t welchie99/scoutsevents-ui:0.0.20-SNAPSHOT-PROD .
->>>>>>> Stashed changes
+docker image build -t welchie99/scoutsevents-ui:0.0.21-SNAPSHOT-PROD .
